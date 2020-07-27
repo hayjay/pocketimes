85cfea8696727a76893453010eb3a2d352ad2032
Nurudeen Ajayi <ydeveloper15@gmail.com>
2020-07-27 02:43:30 +0100

    Added react-redux to the app

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'; //useful to allow redux store (createStore to interact with our application)
+import rootReducer from './reducers/rootReducer';
+//create redux store here
+//since reducers takes data from the dispatch and components sends data to the dispatch action, then rootReducers needs to be connected to the store
+//because its the one that sends the data received to the redux-store. see below 
+const store = createStore(rootReducer);
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* wrap Provide with our app element so we can pass store into our application */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
